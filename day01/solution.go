8773727a3c1f7722861f275dccf77124750a371b
Young Jun Choi <yjunchoi93@gmail.com>
2021-11-30 22:24:14 -0600

    Add template file

---
+++ day01/solution.go
@@ -0,0 +1,9 @@
+package day01
+
+func Part1(input string) int {
+	return 0
+}
+
+func Part2(input string) int {
+	return 0
+}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/WHYjun/advent-of-code-2021/day01"
 	"github.com/WHYjun/advent-of-code-2021/utils"
 )
 
@@ -13,6 +14,9 @@ func main() {
 	fmt.Printf("Running day %02d\n", day)
 
 	switch day {
+	case 1:
+		fmt.Printf("part 1: %d\n", day01.Part1(utils.MustReadFile(day)))
+		fmt.Printf("part 2: %d\n", day01.Part2(utils.MustReadFile(day)))
 	default:
 		utils.PanicError(fmt.Errorf("no such day: %d", day))
 	}
