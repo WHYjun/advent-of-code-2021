d72d77e9a087be47621067b4fbd4dbd3377c2b92
Young Jun Choi <yjunchoi93@gmail.com>
2021-12-15 19:41:12 -0600

    Add SortString function

---
+++ utils/sort.go
@@ -0,0 +1,24 @@
+package utils
+
+import "sort"
+
+// https://stackoverflow.com/a/22698017/9711275
+type sortRunes []rune
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortRunes) Len() int {
+	return len(s)
+}
+
+func SortString(s string) string {
+	r := []rune(s)
+	sort.Sort(sortRunes(r))
+	return string(r)
+}
